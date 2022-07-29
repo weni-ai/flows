80731fe5678d88298f5ae1112c361a85ab0ec073
Sandro Meireles <sandro.meirelesdev@gmail.com>
2022-07-29 01:38:02 -0300

    test: Add logs to trim_http_logs_task task

--- temba/request_logs/tasks.py
+++ temba/request_logs/tasks.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.conf import settings
 from django.utils import timezone
 
@@ -7,9 +9,20 @@ from temba.utils.celery import nonoverlapping_task
 from .models import HTTPLog
 
 
+logger = logging.getLogger(__name__)
+
+
 @nonoverlapping_task(track_started=True, name="trim_http_logs_task")
 def trim_http_logs_task():
     trim_before = timezone.now() - settings.RETENTION_PERIODS["httplog"]
+
+    logger.info(f"[ Trim HTTPLogs ] Clearing all HTTPLogs before: {trim_before}")
+
     ids = HTTPLog.objects.filter(created_on__lte=trim_before).values_list("id", flat=True)
+
+    logger.info("[ Trim HTTPLogs ] All IDs were returned!")
+
     for chunk in chunk_list(ids, 1000):
         HTTPLog.objects.filter(id__in=chunk).delete()
+
+    logger.info("[ Trim HTTPLogs ] The task was successfully completed!")
