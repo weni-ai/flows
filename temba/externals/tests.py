8dc722153bb26ab2cd42defe95875d62235af25f
lucaslinhares <lucaslinhares027@gmail.com>
2023-02-28 12:34:58 -0300

    fix: Fix tests and change attribute name to slug

--- temba/externals/tests.py
+++ temba/externals/tests.py
@@ -1,25 +1,43 @@
 import json
+import os
 
 from rest_framework import status
-from temba.externals.views import GetExternalTypes
+from django.test import TestCase
 from rest_framework.test import APITestCase
+from django.urls import reverse
+
+from temba.externals.types import TYPES
+
+action_file = os.path.join(os.path.dirname(__file__), "types/omie/actions.json")
+
+
+class TestGetActions(TestCase):
+    def setUp(self):
+        self.file = open(action_file, encoding="utf-8")
+        self.action = self.file.read()
+
+    def test_get_actions_sucess(self):
+        slug = "omie"
+        self.types = TYPES.get(slug).get_actions()
+        self.assertEqual(self.types, dict(json.loads(self.action)))
 
 
 class TestGetExternalTypes(APITestCase):
+    databases = "__all__"
+
     def setUp(self):
-        with open("/temba/externals/types/omie/actions.json", encoding="utf-8") as actions:
-            self.action = json.load(actions)
+        self.file = open(action_file)
+        self.action = self.file.read()
 
     def test_get_external_types_success(self):
-        request = self.client.get("/externals/omie/actions")
-        response = GetExternalTypes.as_view()(request, type="omie")
+        slug = "omie"
+        response = self.client.get(reverse("actions", args=[slug]))
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data, self.action)
+        self.assertEqual(response.data, dict(json.loads(self.action)))
 
     def test_get_external_types_error(self):
-        request = self.client.get("/externals/omiee/actions")
-        response = GetExternalTypes.as_view()(request, type="omiee")
+        slug = "omiee"
+        response = self.client.get(reverse("actions", args=[slug]))
 
-        self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
-        self.assertEqual(response.data, Exception)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
