02c7bf57eb36e4bc65ebea8f4a08fb09859924ef
lucaslinhares <lucaslinhares027@gmail.com>
2023-05-03 22:19:12 -0300

    remmove ticketer from asseert_feature function in tests

--- temba/flows/tests.py
+++ temba/flows/tests.py
@@ -372,29 +372,29 @@ class FlowTest(TembaTest):
 
         # add a resthook
         Resthook.objects.create(org=flow.org, created_by=self.admin, modified_by=self.admin)
-        assert_features({"ticketer", "resthook"})
+        assert_features({"resthook"})
 
         # add an NLP classifier
         Classifier.objects.create(org=flow.org, config="", created_by=self.admin, modified_by=self.admin)
-        assert_features({"classifier", "ticketer", "resthook"})
+        assert_features({"classifier", "resthook"})
 
         # add a DT One integration
         DTOneType().connect(flow.org, self.admin, "login", "token")
-        assert_features({"airtime", "classifier", "ticketer", "resthook"})
+        assert_features({"airtime", "classifier", "resthook"})
 
         # change our channel to use a whatsapp scheme
         self.channel.schemes = [URN.WHATSAPP_SCHEME]
         self.channel.save()
-        assert_features({"whatsapp", "airtime", "classifier", "ticketer", "resthook"})
+        assert_features({"whatsapp", "airtime", "classifier", "resthook"})
 
         # change our channel to use a facebook scheme
         self.channel.schemes = [URN.FACEBOOK_SCHEME]
         self.channel.save()
-        assert_features({"facebook", "airtime", "classifier", "ticketer", "resthook"})
+        assert_features({"facebook", "airtime", "classifier", "resthook"})
 
         self.setUpLocations()
 
-        assert_features({"facebook", "airtime", "classifier", "ticketer", "resthook", "locations"})
+        assert_features({"facebook", "airtime", "classifier", "resthook", "locations"})
 
     def test_save_revision(self):
         self.login(self.admin)
