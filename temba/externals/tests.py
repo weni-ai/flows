24f7e82ec2dc160facce6920c731e370bd743826
lucaslinhares <lucaslinhares027@gmail.com>
2023-06-02 09:27:51 -0300

    add tests to omie type

--- temba/externals/tests.py
+++ temba/externals/tests.py
@@ -4,10 +4,12 @@ import os
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 
 from temba.externals.types import TYPES
+from temba.externals.types.omie.type import OmieType
 
 action_file = os.path.join(os.path.dirname(__file__), "types/omie/actions.json")
 
@@ -16,11 +18,20 @@ class TestGetActions(TestCase):
     def setUp(self):
         self.file = open(action_file, encoding="utf-8")
         self.action = self.file.read()
+        self.user = User.objects.create_user(username="super", email="super@user.com", password="super")
 
     def test_get_actions_sucess(self):
         slug = "omie"
         self.types = TYPES.get(slug).get_actions()
         self.assertEqual(self.types, dict(json.loads(self.action)))
+
+    def test_is_available_to(self):
+        omie_type = OmieType()
+        user = self.user
+
+        result = omie_type.is_available_to(user)
+
+        self.assertTrue(result)
 
 
 class TestGetExternalTypes(APITestCase):
