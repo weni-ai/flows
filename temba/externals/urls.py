facb2126facc7db46c975e091d43cde9182c37bb
lucaslinhares <lucaslinhares027@gmail.com>
2023-06-30 18:27:25 -0300

    refact externals model to no use ConnectViewBase

--- temba/externals/urls.py
+++ temba/externals/urls.py
@@ -1,19 +1,7 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 
-from .models import ExternalService
-from .views import ExternalServiceCRUDL, GetExternalTypes
-
-service_urls = []
-for external_service_type in ExternalService.get_types():
-    urls = external_service_type.get_urls()
-    for u in urls:
-        u.name = "externals.types.%s.%s" % (external_service_type.slug, u.name)
-
-    if urls:
-        service_urls.append(url("^%s/" % external_service_type.slug, include(urls)))
+from .views import GetExternalTypes
 
 urlpatterns = [
-    url(r"^externals/types/", include(service_urls)),
-    url(r"^", include(ExternalServiceCRUDL().as_urlpatterns())),
     url(r"^externals/(?P<slug>[^/.]+)/actions", GetExternalTypes.as_view(), name="actions"),
 ]
