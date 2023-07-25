069e5802b95e3fcd3e6e26f6ab03b9d3cb4b3cf7
Paulo Bernardo <paulo.bernardo@weni.ai>
2023-07-25 10:27:54 -0300

    Fix/wpp cloud connect (#440)
    * fix: add wpp embedded signup flow
    
    * fix: wpp cloud init with correct id
    
    * fix: remove specific business id requirement

--- temba/channels/types/whatsapp_cloud/views.py
+++ temba/channels/types/whatsapp_cloud/views.py
@@ -66,8 +66,6 @@ class ClaimView(ClaimViewMixin, SmartFormView):
         url = "https://graph.facebook.com/v13.0/debug_token"
         params = {"access_token": f"{app_id}|{app_secret}", "input_token": oauth_user_token}
 
-        unsupported_facebook_business_id = False
-
         response = requests.get(url, params=params)
         if response.status_code != 200:  # pragma: no cover
             context["waba_details"] = []
@@ -78,11 +76,6 @@ class ClaimView(ClaimViewMixin, SmartFormView):
             waba_targets = []
             granular_scopes = response_json.get("data", dict()).get("granular_scopes", [])
             for scope_dict in granular_scopes:
-                if scope_dict["scope"] == "business_management":
-                    for business_id in scope_dict.get("target_ids", []):
-                        if business_id not in settings.ALLOWED_WHATSAPP_FACEBOOK_BUSINESS_IDS:  # pragma: no cover
-                            unsupported_facebook_business_id = True
-
                 if scope_dict["scope"] in ["whatsapp_business_management", "whatsapp_business_messaging"]:
                     waba_targets.extend(scope_dict.get("target_ids", []))
 
@@ -106,9 +99,6 @@ class ClaimView(ClaimViewMixin, SmartFormView):
                 target_waba_details = response_json
 
                 business_id = target_waba_details["on_behalf_of_business_info"]["id"]
-                if business_id not in settings.ALLOWED_WHATSAPP_FACEBOOK_BUSINESS_IDS:  # pragma: no cover
-                    unsupported_facebook_business_id = True
-                    continue
 
                 url = f"https://graph.facebook.com/v13.0/{target_waba}/phone_numbers"
                 params = {"access_token": oauth_user_token}
@@ -123,7 +113,7 @@ class ClaimView(ClaimViewMixin, SmartFormView):
                             display_phone_number=target_phone["display_phone_number"],
                             phone_number_id=target_phone["id"],
                             waba_id=target_waba_details["id"],
-                            currency="USD",
+                            currency=target_waba_details.get("currency", "USD"),
                             business_id=business_id,
                             message_template_namespace=target_waba_details["message_template_namespace"],
                         )
@@ -140,8 +130,6 @@ class ClaimView(ClaimViewMixin, SmartFormView):
         if context["form"].errors:
             claim_error = context["form"].errors["__all__"][0]
         context["claim_error"] = claim_error
-
-        context["unsupported_facebook_business_id"] = unsupported_facebook_business_id
 
         # make sure we clear the session credentials if no number was granted
         if not context.get("phone_numbers", []):
