248e00f226faa244f8c621fbc512ecd48e0c21f4
lucaslinhares <lucaslinhares027@gmail.com>
2023-09-01 19:21:00 -0300

    adjust project app according to class EDAConsume

--- temba/projects/handle.py
+++ temba/projects/handle.py
@@ -3,5 +3,5 @@ import amqp
 from .consumers import TemplateTypeConsumer, ProjectConsumer
 
 def handle_consumers(channel: amqp.Channel):
-    channel.basic_consume("flows.template-types", callback=TemplateTypeConsumer.consume)
-    channel.basic_consume("flows.projects", callback=ProjectConsumer.consume)
+    channel.basic_consume("flows.template-types", callback=TemplateTypeConsumer().handle)
+    channel.basic_consume("flows.projects", callback=ProjectConsumer().handle)
