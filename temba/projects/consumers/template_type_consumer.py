b2a19f8becb72e33a6e654526582550531225f6e
lucaslinhares <lucaslinhares027@gmail.com>
2023-09-06 00:09:21 -0300

    run black and isort in template type

--- temba/projects/consumers/template_type_consumer.py
+++ temba/projects/consumers/template_type_consumer.py
@@ -1,9 +1,10 @@
 import amqp
-from temba.event_driven.consumers import EDAConsumer
 from sentry_sdk import capture_exception
 
+from temba.event_driven.consumers import EDAConsumer
 from temba.event_driven.parsers.json_parser import JSONParser
 from temba.projects.usecases.template_type_creation import create_template_type
+
 
 class TemplateTypeConsumer(EDAConsumer):
     def consume(self, message: amqp.Message):  # pragma: no cover
@@ -14,11 +15,7 @@ class TemplateTypeConsumer(EDAConsumer):
 
             message.channel.basic_ack(message.delivery_tag)
 
-
         except Exception as exception:
             capture_exception(exception)
             message.channel.basic_reject(message.delivery_tag, requeue=False)
             print(f"[TemplateTypeConsumer] - Message rejected by: {exception}")
-            return None
-
-        message.channel.basic_ack(message.delivery_tag)
--- temba/projects/usecases/template_type_creation.py
+++ temba/projects/usecases/template_type_creation.py
@@ -1,9 +1,9 @@
 import itertools
 
+from weni.internal.models import Project
+
 from temba.campaigns.models import Campaign
 from temba.projects.models import TemplateType
-
-from weni.internal.models import Project
 
 
 def create_template_type(uuid: str, name: str, project_uuid: Project) -> TemplateType:
@@ -12,7 +12,7 @@ def create_template_type(uuid: str, name: str, project_uuid: Project) -> Templat
 
     campaigns = Campaign.objects.filter(org=project.org, is_active=True)
     components = set(itertools.chain(flows, campaigns))
-    setup = project.export_definitions("https://flows.weni.ai", components) # TODO: find a way to get branding["link"]
+    setup = project.export_definitions("https://flows.weni.ai", components)  # TODO: find a way to get branding["link"]
 
     template_type = TemplateType(uuid=uuid, name=name, setup=setup)
     template_type.save()
