e18fda3daa8c0c5e150244f1512d4859c91f263e
lucaslinhares <lucaslinhares027@gmail.com>
2023-09-13 16:26:33 -0300

    add handle consumers in event_driven handle

--- temba/event_driven/handle.py
+++ temba/event_driven/handle.py
@@ -1,5 +1,11 @@
 from amqp.channel import Channel  # pragma: no cover
 
+from temba.classifiers.handle import handle_consumers as classifier_handle_consumers  # pragma: no cover
+from temba.projects.handle import handle_consumers as project_handle_consumers  # pragma: no cover
+from temba.tickets.handle import handle_consumers as ticketer_handle_consumers  # pragma: no cover
+
 
 def handle_consumers(channel: Channel) -> None:  # pragma: no cover
-    pass
+    project_handle_consumers(channel)
+    classifier_handle_consumers(channel)
+    ticketer_handle_consumers(channel)
