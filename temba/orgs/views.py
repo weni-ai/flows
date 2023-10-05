d30e8b8c275ef5547887f580bfc12c466b24b96f
Paulo Bernardo <paulo.bernardo@weni.ai>
2023-10-05 13:22:01 -0300

    fix: run code_check

--- temba/orgs/views.py
+++ temba/orgs/views.py
@@ -3248,7 +3248,6 @@ class OrgCRUDL(SmartCRUDL):
             user = self.request.user
             org = user.get_org()
 
-
             # if we are on the topups plan, show our usual credits view
             if org.plan == settings.TOPUP_PLAN:
                 if self.has_org_perm("orgs.topup_list"):
