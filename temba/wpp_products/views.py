deea05e0ce4785a95f93aa65fdd0997e277d68eb
lucaslinhares <lucaslinhares027@gmail.com>
2023-11-24 15:23:25 -0300

    Add endpoints to create catalog and product

--- temba/wpp_products/views.py
+++ temba/wpp_products/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from weni.internal.views import InternalGenericViewSet
 
 from temba.channels.models import Channel
-from temba.utils.whatsapp.tasks import update_channel_catalogs_status
+from temba.utils.whatsapp.tasks import update_channel_catalogs_status, update_local_catalogs, update_local_products
 from temba.wpp_products.serializers import UpdateCatalogSerializer
 
 
@@ -21,4 +21,18 @@ class CatalogViewSet(viewsets.ViewSet, InternalGenericViewSet):
         validated_data = serializer.validated_data
 
         update_channel_catalogs_status(self.get_object(), validated_data.get("facebook_catalog_id"))
+        return Response(status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=["POST"], url_path="create-catalog")
+    def create_catalog(self, request, *args, **kwargs):
+        catalogs = request.data.get("catalogs")
+        for catalog in catalogs:
+            update_local_catalogs(catalog)
+        return Response(status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=["POST"], url_path="create-product")
+    def create_product(self, request, *args, **kwargs):
+        products = request.data.get("products")
+        for product in products:
+            update_local_products(product)
         return Response(status=status.HTTP_200_OK)
