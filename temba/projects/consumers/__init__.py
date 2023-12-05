4b3e199c8e4abaa15f944a35ba0eb69cacd338d9
lucaslinhares <lucaslinhares027@gmail.com>
2023-12-05 11:29:00 -0300

    Add description in create project and create update project queue

--- temba/projects/consumers/__init__.py
+++ temba/projects/consumers/__init__.py
@@ -1,2 +1,3 @@
 from .project_consumer import ProjectConsumer  # noqa
 from .template_type_consumer import TemplateTypeConsumer  # noqa
+from .update_project_consumer import UpdateProjectConsumer  # noqa
