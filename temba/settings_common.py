e3fc80dee39300e1de862bb58cec92946439cef6
lucaslinhares <lucaslinhares027@gmail.com>
2024-01-31 18:55:47 -0300

    revert default_database_config to temba

--- temba/settings_common.py
+++ temba/settings_common.py
@@ -969,9 +969,9 @@ TEST_EXCLUDE = ("smartmin",)
 # -----------------------------------------------------------------------------------
 _default_database_config = {
     "ENGINE": "django.contrib.gis.db.backends.postgis",
-    "NAME": "rapidpro",
-    "USER": "rapidpro",
-    "PASSWORD": "rapidpro",
+    "NAME": "temba",
+    "USER": "temba",
+    "PASSWORD": "temba",
     "HOST": "localhost",
     "PORT": "5432",
     "ATOMIC_REQUESTS": True,
@@ -1339,4 +1339,5 @@ SENTENX_URL = os.environ.get("SENTENX_URL", default="")
 # Intelligence Token
 INTELLIGENCES_TOKEN = os.environ.get("INTELLIGENCES_TOKEN", default="")
 
+# Nexus url
 NEXUS_BASE_URL = os.environ.get("NEXUS_BASE_URL", default="https://nexus.dev.cloud.weni.ai")
