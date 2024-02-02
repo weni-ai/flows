47663d76c00cf8a6839cd938921b7cf604d02d4d
lucaslinhares <lucaslinhares027@gmail.com>
2024-02-02 16:32:57 -0300

    add test for intelligence endpoint

--- temba/api/v2/wenigpt/tests.py
+++ temba/api/v2/wenigpt/tests.py
@@ -20,6 +20,7 @@ class TestIntelligencesEndpoint(TembaTest):
         ]
 
         mock_requests_get.return_value = mock_response
+        mock_requests_get.return_value.status_code = 200
 
         url = reverse("api.v2.intelligences") + ".json"
 
@@ -35,5 +36,33 @@ class TestIntelligencesEndpoint(TembaTest):
                 {"id": "654321", "name": "Test Content Base 1", "intelligence": "Test Intelligence 0"},
             ]
         }
+
+        self.assertEqual(response.json(), expected_data)
+
+    @patch("temba.api.v2.wenigpt.views.requests.get")
+    def test_get_intelligences_error(self, mock_requests_get):
+        mock_response = MagicMock()
+        mock_response.json.return_value = [
+            {
+                "intelligence_name": "Test Intelligence 0",
+                "content_bases": [
+                    {"uuid": "123456", "content_base_name": "Test Content Base 0"},
+                    {"uuid": "654321", "content_base_name": "Test Content Base 1"},
+                ],
+            },
+        ]
+
+        mock_requests_get.return_value = mock_response
+        mock_requests_get.return_value.status_code = 500
+
+        url = reverse("api.v2.intelligences") + ".json"
+
+        self.client.force_login(self.user)
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+        expected_data = {"results": []}
 
         self.assertEqual(response.json(), expected_data)
