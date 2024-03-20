8462ec57f4db42815345c6d35f8de74adb9e6632
lucaslinhares <lucaslinhares027@gmail.com>
2024-03-20 12:43:27 -0300

    testing pagination elastic

--- temba/api/v2/elasticsearch/views.py
+++ temba/api/v2/elasticsearch/views.py
@@ -87,26 +87,19 @@ class ContactsElasticSearchEndpoint(APIView):
 
             from_index = (page_number - 1) * page_size
 
-            contacts = Search(using=client, index=index).query(
-                qs
-            )  # .params(size=page_size, from_=(page_number - 1) * page_size)
+            contacts = Search(using=client, index=index).query(qs).params(size=page_size, from_=from_index)
             response = list(contacts.scan())
-
-            for _ in range(from_index):
-                next(response, None)
-
+            
             results = [hit.to_dict() for hit in response]
 
-            results = results[:page_size]
-
-            total_results = len(results)
+            total_results = response.hits.total.value if hasattr(response.hits, 'total') and hasattr(response.hits.total, 'value') else len(results)
             total_pages = ceil(total_results / page_size)
 
             new_url = request.build_absolute_uri()
             pagination_links = get_pagination_links(new_url, page_number, total_pages, page_size)
 
             data = {
-                "results": results,
+                "results": [hit.to_dict() for hit in results],
                 "pagination": {
                     "page_number": page_number,
                     "page_size": page_size,
