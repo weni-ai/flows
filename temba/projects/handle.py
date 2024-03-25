3ef0b59458a7b7eec57a8d34341babef6690898d
lucaslinhares <lucaslinhares027@gmail.com>
2024-03-25 19:52:31 -0300

    Create consumer to update user permissions

--- temba/projects/handle.py
+++ temba/projects/handle.py
@@ -1,9 +1,15 @@
 import amqp
 
-from .consumers import ProjectConsumer, TemplateTypeConsumer, UpdateProjectConsumer  # pragma: no cover
+from .consumers import (  # pragma: no cover
+    ProjectConsumer,
+    TemplateTypeConsumer,
+    UpdatePermissionConsumer,
+    UpdateProjectConsumer,
+)
 
 
 def handle_consumers(channel: amqp.Channel):
     channel.basic_consume("flows.template-types", callback=TemplateTypeConsumer().handle)
     channel.basic_consume("flows.projects", callback=ProjectConsumer().handle)
     channel.basic_consume("flows.update-project", callback=UpdateProjectConsumer().handle)  # pragma: no cover
+    channel.basic_consume("flows.update-permission", callback=UpdatePermissionConsumer().handle)
