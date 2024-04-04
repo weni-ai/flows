af951307173b122d2cb1b7619e966f4ebc0907ed
sandro.meireles <sandro.meirelesdev@gmail.com>
2024-04-04 12:21:56 -0300

    feat: Create internal andpoint that allows to create flow starts

---
+++ temba/api/v2/internals/flows/urls.py
@@ -0,0 +1,9 @@
+from rest_framework import routers
+
+from .views import FlowStartViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r"flow_starts", FlowStartViewSet, basename="flow_starts")
+
+urlpatterns = router.urls
