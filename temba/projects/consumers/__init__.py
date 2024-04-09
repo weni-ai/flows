28a069ef14cfb19e958e66973edbe8d27a08685c
lucaslinhares <lucaslinhares027@gmail.com>
2024-04-09 10:45:49 -0300

    consumer to update brain on

--- temba/projects/consumers/__init__.py
+++ temba/projects/consumers/__init__.py
@@ -1,3 +1,4 @@
 from .project_consumer import ProjectConsumer  # noqa
 from .template_type_consumer import TemplateTypeConsumer  # noqa
+from .update_brain_on_consumer import UpdateBrainOnConsumer  # noqa
 from .update_project_consumer import UpdateProjectConsumer  # noqa
