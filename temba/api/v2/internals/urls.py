d8e4d83965da99c05baaaa68f746cf34dccac548
sandro.meireles <sandro.meirelesdev@gmail.com>
2024-05-22 17:55:29 -0300

    fix: running isort in temba.api.v2.internals.urls

--- temba/api/v2/internals/urls.py
+++ temba/api/v2/internals/urls.py
@@ -1,6 +1,6 @@
 from .broadcasts.urls import urlpatterns as broadcasts_urlpatterns
-from .flows.urls import urlpatterns as flows_urlpatterns
 from .channels.urls import urlpatterns as channels_urlpatterns
+from .flows.urls import urlpatterns as flows_urlpatterns
 
 urlpatterns = []
 urlpatterns += broadcasts_urlpatterns
