bceca0ac33937f9c71fe1c92c772965f02aa4952
Lucas <lucas.linhares@weni.ai>
2024-08-20 19:34:51 -0300

    Add consumer to integrate feature template (#372)
    * Add consumer to integrate feature template

---
+++ temba/features/consumers/feature_template_integration_consumer.py
@@ -0,0 +1,27 @@
+import amqp
+from sentry_sdk import capture_exception
+
+from temba.event_driven.consumers import EDAConsumer
+from temba.event_driven.parsers.json_parser import JSONParser
+from temba.features.usecases.feature_template_integration import integrate_feature_template_consumer
+
+
+class IntegrateFeatureTemplateConsumer(EDAConsumer):
+    def consume(self, message: amqp.Message):  # pragma: no cover
+        print("[IntegrateFeatureTemplateConsumer] - Consuming a message.")
+        try:
+            body = JSONParser.parse(message.body)
+            integrate_feature_template_consumer(
+                project_uuid=body.get("project_uuid"),
+                feature_version_uuid=body.get("feature_version"),
+                definition=body.get("definition"),
+                parameters=body.get("parameters"),
+                user_email=body.get("user_email"),
+            )
+
+            message.channel.basic_ack(message.delivery_tag)
+
+        except Exception as exception:
+            capture_exception(exception)
+            message.channel.basic_reject(message.delivery_tag, requeue=False)
+            print(f"[IntegrateFeatureTemplateConsumer] - Message rejected by: {exception}")
---
+++ temba/features/handle.py
@@ -0,0 +1,8 @@
+import amqp  # pragma: no cover
+
+from .consumers import DeleteFeatureTemplateIntegrationConsumer, IntegrateFeatureTemplateConsumer  # pragma: no cover
+
+
+def handle_consumers(channel: amqp.Channel):  # pragma: no cover
+    channel.basic_consume("flows.integrate-feature-template", callback=IntegrateFeatureTemplateConsumer().handle)
+    channel.basic_consume("flows.delete-feature", callback=DeleteFeatureTemplateIntegrationConsumer().handle)
