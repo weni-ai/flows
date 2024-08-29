789edc39431d9e37e2708b67cd48db4bb45137d2
sandro.meireles <sandro.meirelesdev@gmail.com>
2024-08-29 11:27:45 -0300

    feat: Adjust create_ticketer to allow updating ticketers

--- temba/tickets/usecases/ticketer_creation.py
+++ temba/tickets/usecases/ticketer_creation.py
@@ -35,60 +35,74 @@ def list_flow_definition_ticketers(flow_definition) -> list:
     return ticketers
 
 
+def update_flow_definition(integration_request, ticketer):
+    flow = integration_request.flow
+    last_revision = FlowRevision.objects.filter(flow=flow).last()
+    definition = last_revision.definition
+
+    dumps_definition = json.dumps(definition)
+    updated_definition = dumps_definition.replace(
+        str(integration_request.integration_uuid),
+        str(ticketer.uuid),
+    )
+
+    flow_definition_ticketers = list_flow_definition_ticketers(definition)
+
+    for flow_definition_ticketer in flow_definition_ticketers:
+        topic = flow_definition_ticketer.get("topic", {})
+        topic_uuid = topic.get("uuid")
+        topic_name = topic.get("name")
+        queue = ticketer.queues.get(name=topic_name)
+        updated_definition = updated_definition.replace(
+            str(topic_uuid), str(queue.uuid)
+        )
+
+    loads_definition = json.loads(updated_definition)
+    last_revision.definition = loads_definition
+    last_revision.save()
+
+
 def create_ticketer(
-    project_auth: str, name: str, project_uuid: str, user_email: str, uuid: str, queues: list
+    project_auth: str,
+    name: str,
+    project_uuid: str,
+    user_email: str,
+    uuid: str,
+    queues: list,
 ) -> Ticketer:
     project = Project.objects.get(project_uuid=project_uuid)
     integration_requests = IntegrationRequest.objects.filter(project=project, name=name)
 
-    user, created = User.objects.get_or_create(email=user_email)
+    user, _ = User.objects.get_or_create(email=user_email)
 
-    ticketer = Ticketer.objects.create(
+    ticketer, _ = Ticketer.objects.get_or_create(
         uuid=uuid,
-        org=project.org,
-        ticketer_type="wenichats",
-        created_by=user,
-        modified_by=user,
-        name=name,
-        config=dict(
-            project_auth=project_auth,
-            sector_uuid=uuid,
+        defaults=dict(
+            org=project.org,
+            ticketer_type="wenichats",
+            created_by=user,
+            modified_by=user,
+            name=name,
+            config=dict(
+                project_auth=project_auth,
+                sector_uuid=uuid,
+            ),
         ),
     )
 
     for queue in queues:
-        TicketerQueue.objects.create(
+        TicketerQueue.objects.get_or_create(
             uuid=queue.get("uuid"),
-            name=queue.get("name"),
-            org=project.org,
-            created_by=user,
-            modified_by=user,
-            ticketer=ticketer,
+            defaults=dict(
+                name=queue.get("name"),
+                org=project.org,
+                created_by=user,
+                modified_by=user,
+                ticketer=ticketer,
+            ),
         )
 
     for integration_request in integration_requests:
-        flow = integration_request.flow
-        last_revision = FlowRevision.objects.filter(flow=flow).last()
-        definition = last_revision.definition
-
-        dumps_definition = json.dumps(definition)
-        updated_definition = dumps_definition.replace(
-            str(integration_request.integration_uuid),
-            str(ticketer.uuid),
-        )
-
-        flow_definition_ticketers = list_flow_definition_ticketers(definition)
-
-        for flow_definition_ticketer in flow_definition_ticketers:
-            topic = flow_definition_ticketer.get("topic", {})
-            topic_uuid = topic.get("uuid")
-            topic_name = topic.get("name")
-
-            queue = ticketer.queues.get(name=topic_name)
-            updated_definition = updated_definition.replace(str(topic_uuid), str(queue.uuid))
-
-        loads_definition = json.loads(updated_definition)
-        last_revision.definition = loads_definition
-        last_revision.save()
+        update_flow_definition(integration_request, ticketer)
 
     return ticketer
