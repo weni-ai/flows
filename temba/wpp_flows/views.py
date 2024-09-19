255ea3de31c308546a459ee9218d6a611e3d3405
lucaslinhares <lucaslinhares027@gmail.com>
2024-09-19 18:01:06 -0300

    refact views code

--- temba/wpp_flows/views.py
+++ temba/wpp_flows/views.py
@@ -1,8 +1,11 @@
 from rest_framework import status, viewsets
+from rest_framework.exceptions import AuthenticationFailed, NotAuthenticated
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-from temba.wpp_flows.utils import update_whatsapp_flows_status
+from django.conf import settings
+
+from temba.wpp_flows.models import WhatsappFlow
 
 
 class WhatsappFlowsViewset(viewsets.ViewSet):
@@ -10,7 +13,35 @@ class WhatsappFlowsViewset(viewsets.ViewSet):
     permission_classes = [AllowAny]
 
     def create(self, request):
-        update_status = update_whatsapp_flows_status(request.data)
-        if not update_status:
-            return Response("An error occured", status=status.HTTP_400_BAD_REQUEST)
+        payload = request.data
+        params = request.query_params
+        token = params.get("token")
+
+        if token is None:
+            raise NotAuthenticated()
+
+        if token != settings.COURIER_FIXED_ACCESS_TOKEN:
+            raise AuthenticationFailed()
+
+        entry_list = payload.get("entry", None)
+
+        if entry_list is None:
+            return Response("`entry` is a required field", status=status.HTTP_400_BAD_REQUEST)
+
+        entry = entry_list[0]
+
+        flow_id = entry.get("id")
+        if flow_id is None:
+            return Response("`id` is a required field", status=status.HTTP_400_BAD_REQUEST)
+
+        new_status = entry["changes"][0]["value"]["new_status"]
+
+        if not WhatsappFlow.is_status_valid(new_status):
+            return Response("status is not valid", status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            WhatsappFlow.update_status(flow_id, new_status)
+        except WhatsappFlow.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
         return Response(status=status.HTTP_200_OK)
