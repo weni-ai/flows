551e4f83bb72283b803db8acda4dbec47a324fe5
Lucas <lucas.linhares@weni.ai>
2024-10-10 15:53:38 -0300

    Email channel type (#409)
    * Email channel type

---
+++ temba/channels/types/email/tests.py
@@ -0,0 +1,53 @@
+from django.conf import settings
+from django.urls import reverse
+
+from temba.channels.models import Channel
+from temba.tests.base import TembaTest
+
+
+class EmailTypeTest(TembaTest):
+    def test_claim(self):
+        claim_url = reverse("channels.types.email.claim")
+
+        self.login(self.admin)
+
+        response = self.client.get(reverse("channels.channel_claim"))
+        self.assertContains(response, claim_url)
+
+        response = self.client.get(claim_url)
+        self.assertEqual(200, response.status_code)
+
+        response = self.client.post(
+            claim_url,
+            {
+                "user_name": "email@gmail.com",
+                "password": "*******",
+                "smtp_host": "smtp.gmail.com",
+                "smtp_port": 587,
+                "imap_host": "imap.gmail.com",
+                "imap_port": 993,
+                "token": "token",
+                "refresh_token": "refresh_token",
+            },
+            follow=True,
+        )
+        self.assertEqual(200, response.status_code)
+
+        channel = Channel.objects.get(channel_type="EM")
+        self.assertEqual("email@gmail.com", channel.name)
+        self.assertEqual("email@gmail.com", channel.address)
+        self.assertEqual(
+            {
+                "user_name": "email@gmail.com",
+                "password": "*******",
+                "smtp_host": "smtp.gmail.com",
+                "smtp_port": 587,
+                "imap_host": "imap.gmail.com",
+                "imap_port": 993,
+                "loc": "",
+                "token": "token",
+                "refresh_token": "refresh_token",
+                "callback_domain": settings.HOSTNAME,
+            },
+            channel.config,
+        )
