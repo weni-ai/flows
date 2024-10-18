784b29b1ee033a4cf7936f34ae98c7bb21fd3067
lucaslinhares <lucaslinhares027@gmail.com>
2024-10-18 17:54:02 -0300

    Update migration to update new_id in batchs

--- temba/request_logs/migrations/0018_http_id_bigint.py
+++ temba/request_logs/migrations/0018_http_id_bigint.py
@@ -10,10 +10,41 @@ BEGIN
     RETURN NEW;
 END
 $BODY$ LANGUAGE PLPGSQL;
-CREATE TRIGGER set_new_id_trigger BEFORE INSERT OR UPDATE ON public.request_logs_httplog
+
+CREATE TRIGGER set_new_id_trigger
+BEFORE INSERT OR UPDATE ON public.request_logs_httplog
 FOR EACH ROW EXECUTE PROCEDURE set_new_id();
 
-UPDATE public.request_logs_httplog SET new_id = id;
+DO $$
+DECLARE
+    batch_size INTEGER := 10000;   -- Define o tamanho do batch
+    min_id BIGINT;                 -- Armazena o menor valor de id no batch atual
+    max_id BIGINT;                 -- Armazena o maior valor de id na tabela
+    rows_updated INTEGER;          -- Contador de linhas atualizadas em cada batch
+BEGIN
+
+    SELECT MIN(id) INTO min_id FROM public.request_logs_httplog;
+
+    SELECT MAX(id) INTO max_id FROM public.request_logs_httplog;
+
+    WHILE min_id <= max_id LOOP
+
+        UPDATE public.request_logs_httplog
+        SET new_id = id
+        WHERE id >= min_id AND id < min_id + batch_size
+        AND new_id IS DISTINCT FROM id;
+
+        GET DIAGNOSTICS rows_updated = ROW_COUNT;
+
+        IF rows_updated = 0 THEN
+            EXIT;
+        END IF;
+
+        min_id := min_id + batch_size;
+
+        PERFORM pg_sleep(0.1);
+    END LOOP;
+END $$;
 
 CREATE UNIQUE INDEX IF NOT EXISTS new_id_unique ON public.request_logs_httplog(new_id);
 
@@ -21,24 +52,22 @@ ALTER TABLE public.request_logs_httplog ADD CONSTRAINT new_id_not_null CHECK (ne
 
 ALTER TABLE public.request_logs_httplog VALIDATE CONSTRAINT new_id_not_null;
 
-
 BEGIN TRANSACTION;
 
--- explicitly lock the table against other changes (safety)
 LOCK TABLE public.request_logs_httplog IN EXCLUSIVE MODE;
 
--- drop and create the PK using existing index
-ALTER TABLE public.request_logs_httplog DROP CONSTRAINT request_logs_httplog_pkey, ADD CONSTRAINT request_logs_httplog_pkey PRIMARY KEY USING INDEX new_id_unique;
+ALTER TABLE public.request_logs_httplog
+DROP CONSTRAINT request_logs_httplog_pkey,
+ADD CONSTRAINT request_logs_httplog_pkey PRIMARY KEY USING INDEX new_id_unique;
 
--- transfer the sequence
 ALTER SEQUENCE public.request_logs_httplog_id_seq OWNED BY public.request_logs_httplog.new_id;
+
 ALTER TABLE public.request_logs_httplog ALTER COLUMN new_id SET DEFAULT nextval('public.request_logs_httplog_id_seq');
 
--- drop and rename the columns
 ALTER TABLE public.request_logs_httplog DROP COLUMN id;
+
 ALTER TABLE public.request_logs_httplog RENAME COLUMN new_id TO id;
 
--- drop the temporary trigger and procedure
 DROP TRIGGER IF EXISTS set_new_id_trigger ON public.request_logs_httplog;
 DROP FUNCTION IF EXISTS set_new_id();
 
