e376033ede4ace5d4921c7f939580faad08d2605
lucaslinhares <lucaslinhares027@gmail.com>
2025-01-28 18:57:07 -0300

    Run code check

--- temba/api/v2/validators.py
+++ temba/api/v2/validators.py
@@ -1,10 +1,9 @@
+import requests
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.validators import UniqueValidator, qs_filter
 
 from django.conf import settings
-
-import requests
 
 
 class UniqueForOrgValidator(UniqueValidator):
