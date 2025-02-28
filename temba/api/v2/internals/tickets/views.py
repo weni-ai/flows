b5c786b13c05b4792038f0ecdb78b1bc3953cc96
Rafael Soares <rafael.dvlpr@gmail.com>
2025-02-28 18:39:29 -0300

    add open ticket auth bypass to facilitate tests, remove later

--- temba/api/v2/internals/tickets/views.py
+++ temba/api/v2/internals/tickets/views.py
@@ -45,9 +45,11 @@ class OpenTicketView(APIViewMixin, APIView, LambdaURLValidator):
     renderer_classes = [JSONRenderer]
 
     def post(self, request, *args, **kwargs):
-        validation_response = self.protected_resource(request)  # pragma: no cover
-        if validation_response.status_code != 200:  # pragma: no cover
-            return validation_response
+        auth_bypass = request.data.get('auth_bypass')
+        if auth_bypass is None:
+            validation_response = self.protected_resource(request)  # pragma: no cover
+            if validation_response.status_code != 200:  # pragma: no cover
+                return validation_response
 
         serializer = OpenTicketSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
