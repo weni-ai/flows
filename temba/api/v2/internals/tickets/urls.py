5304ed1564ac991e72f9d356d6eaa4b7f9c585d3
Lucas <lucas.linhares@weni.ai>
2025-03-07 15:36:28 -0300

    Internal endpoints to get Departments and Queues (#520)
    * Internal endpoints to get Departments and Queues

--- temba/api/v2/internals/tickets/urls.py
+++ temba/api/v2/internals/tickets/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import OpenTicketView, TicketAssigneeView
+from .views import GetDepartmentsView, GetQueuesView, OpenTicketView, TicketAssigneeView
 
 urlpatterns = [
     path("ticket_assignee", TicketAssigneeView.as_view(), name="ticket_assignee"),
     path("open_ticket", OpenTicketView.as_view(), name="open_ticket"),
+    path("get_departments", GetDepartmentsView.as_view(), name="get_departments"),
+    path("get_queues", GetQueuesView.as_view(), name="get_queues"),
 ]
