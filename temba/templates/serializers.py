55068c51d763cddf67b3b1feeb9ff075ccb7fa57
Eliton Jorge <eliton-jorge@hotmail.com>
2025-03-17 16:06:58 -0300

    fix: refactor template sync logic and webhook validation (#527)

--- temba/templates/serializers.py
+++ temba/templates/serializers.py
@@ -2,6 +2,6 @@ from rest_framework import serializers
 
 
 class TemplateSyncSerializer(serializers.Serializer):
-    webhook = serializers.DictField(required=True)
+    webhook = serializers.DictField(required=False)
     template_data = serializers.DictField(required=True)
     template_name = serializers.CharField(required=True)
