4a42a226f12871bec26a19f87975f1038391a83b
lucaslinhares <lucaslinhares027@gmail.com>
2025-05-22 19:10:00 -0300

    Running code check

--- temba/api/v2/serializers.py
+++ temba/api/v2/serializers.py
@@ -287,9 +287,7 @@ class WhatsappBroadcastWriteSerializer(WriteSerializer):
     channel = serializers.UUIDField(required=False)
 
     def validate_msg(self, value):
-        if not (
-            value.get("text") or value.get("attachments") or value.get("template") or value.get("action_type")
-        ):
+        if not (value.get("text") or value.get("attachments") or value.get("template") or value.get("action_type")):
             raise serializers.ValidationError("Must provide either text, attachments, template or action_type")
         return value
 
