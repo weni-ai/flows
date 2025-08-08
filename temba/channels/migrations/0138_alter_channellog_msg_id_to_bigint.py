cef521f7993a1b8ad1433633498931d081000cb6
Rafael Soares <rafael.dvlpr@gmail.com>
2025-08-08 12:16:10 -0300

    add channellog msg_id to bigint migration

---
+++ temba/channels/migrations/0138_alter_channellog_msg_id_to_bigint.py
@@ -0,0 +1,24 @@
+# Generated manually
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("channels", "0137_alter_channel_name"),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            """
+            ALTER TABLE channels_channellog 
+            ALTER COLUMN msg_id TYPE bigint 
+            USING msg_id::bigint;
+            """,
+            """
+            ALTER TABLE channels_channellog 
+            ALTER COLUMN msg_id TYPE integer 
+            USING msg_id::integer;
+            """
+        ),
+    ]
