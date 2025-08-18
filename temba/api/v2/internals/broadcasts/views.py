a628346cc1c63bf08935719669b12d6509a18410
lucaslinhares <lucaslinhares027@gmail.com>
2025-08-18 16:13:50 -0300

    Add permission class IsUserInOrg

--- temba/api/v2/internals/broadcasts/views.py
+++ temba/api/v2/internals/broadcasts/views.py
@@ -1,21 +1,21 @@
 from rest_framework.exceptions import AuthenticationFailed, NotAuthenticated
 from rest_framework.mixins import CreateModelMixin
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from weni.internal.authenticators import InternalOIDCAuthentication
 from weni.internal.permissions import CanCommunicateInternally
 from weni.internal.views import InternalGenericViewSet
-from rest_framework.pagination import LimitOffsetPagination
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
 
+from temba.api.v2.internals.org_permission import IsUserInOrg
 from temba.api.v2.internals.views import APIViewMixin
 from temba.api.v2.serializers import WhatsappBroadcastWriteSerializer
 from temba.msgs.models import Broadcast, BroadcastStatistics
 from temba.orgs.models import Org
-from temba.api.v2.views_base import CreatedOnCursorPagination
 
 from .serializers import BroadcastSerializer, BroadcastWithStatisticsSerializer, UserAndProjectSerializer
 
@@ -80,7 +80,7 @@ class InternalWhatsappBroadcastsEndpoint(APIViewMixin, APIView):
 
 class InternalBroadcastStatisticsEndpoint(APIViewMixin, APIView):
     authentication_classes = [InternalOIDCAuthentication]
-    permission_classes = [IsAuthenticated, CanCommunicateInternally]
+    permission_classes = [IsAuthenticated, IsUserInOrg]
     serializer_class = BroadcastWithStatisticsSerializer
 
     class Pagination(LimitOffsetPagination):
@@ -120,7 +120,7 @@ class InternalBroadcastStatisticsEndpoint(APIViewMixin, APIView):
 
 class InternalBroadcastStatisticMontlyEndpoint(APIViewMixin, APIView):
     authentication_classes = [InternalOIDCAuthentication]
-    permission_classes = [IsAuthenticated, CanCommunicateInternally]
+    permission_classes = [IsAuthenticated, IsUserInOrg]
 
     def get(self, request, *args, **kwargs):
         project_uuid = request.query_params.get("project_uuid")
