bf5da15061d1e5b7b5f401b9dc5596e14f0e6286
lucaslinhares <lucaslinhares027@gmail.com>
2025-08-21 16:52:19 -0300

    Add endpoint to get Template Translations

---
+++ temba/api/v2/templates/views.py
@@ -0,0 +1,57 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from weni.internal.authenticators import InternalOIDCAuthentication
+from weni.internal.permissions import CanCommunicateInternally
+
+from django.db.models import F
+
+from temba.api.v2.internals.org_permission import IsUserInOrg
+from temba.api.v2.internals.views import APIViewMixin
+from temba.api.v2.views_base import CreatedOnCursorPagination
+from temba.orgs.models import Org
+from temba.templates.models import TemplateTranslation
+
+from .serializers import TemplateTranslationDetailsSerializer
+
+
+class TemplatesTranslationsEndpoint(APIViewMixin, APIView):
+    """
+    GET returns paginated list of active template translations for an org, selected by project_uuid.
+    Each item includes header/body/footer/buttons.
+    Query params:
+      - project_uuid: required
+      - limit: optional page size
+      - cursor: optional cursor for pagination
+    """
+
+    authentication_classes = [InternalOIDCAuthentication]
+    permission_classes = [IsAuthenticated, IsUserInOrg]
+
+    class Pagination(CreatedOnCursorPagination):
+        default_page_size = 20
+        page_size_query_param = "limit"
+        ordering = ("-sort_on", "-id")
+
+    def get(self, request, *args, **kwargs):
+        project_uuid = request.query_params.get("project_uuid")
+        if not project_uuid:
+            return Response({"error": "Project not provided"}, status=401)
+
+        try:
+            org = Org.objects.get(proj_uuid=project_uuid)
+        except Org.DoesNotExist:
+            return Response({"error": "Project not found"}, status=404)
+
+        queryset = (
+            TemplateTranslation.objects.filter(is_active=True, template__org=org, template__is_active=True)
+            .select_related("template")
+            .prefetch_related("headers", "buttons")
+            .annotate(sort_on=F("template__modified_on"))
+            .order_by("-sort_on", "-id")
+        )
+
+        pagination = self.Pagination()
+        page = pagination.paginate_queryset(queryset, request, self)
+        serializer = TemplateTranslationDetailsSerializer(page, many=True)
+        return pagination.get_paginated_response(serializer.data)
