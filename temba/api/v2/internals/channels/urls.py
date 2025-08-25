3757cad0ab67e27b09b8459423737e3178133af4
Lucas <lucas.linhares@weni.ai>
2025-08-25 17:06:33 -0300

    Internal endpoint to get channels (#603)
    * Internal endpoint to get channels

--- temba/api/v2/internals/channels/urls.py
+++ temba/api/v2/internals/channels/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
-from .views import ChannelAllowedDomainsView, ChannelProjectView
+from .views import ChannelAllowedDomainsView, ChannelProjectView, InternalChannelView
 
-urlpatterns = [path("channel_projects", ChannelProjectView.as_view(), name="channel_projects")]
+urlpatterns = [
+    path("channel_projects", ChannelProjectView.as_view(), name="channel_projects"),
+    path("channels-by-project", InternalChannelView.as_view(), name="channels-by-project"),
+]
 
 urlpatterns.append(
-    path("channel_allowed_domains", ChannelAllowedDomainsView.as_view(), name="channel_allowed_domains")
+    path(
+        "channel_allowed_domains",
+        ChannelAllowedDomainsView.as_view(),
+        name="channel_allowed_domains",
+    )
 )
