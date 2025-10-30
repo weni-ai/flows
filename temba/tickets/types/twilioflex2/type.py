9bcfab9a09f68c1e99284f9e20906a039a7a9748
Rafael Soares <rafael.dvlpr@gmail.com>
2025-10-30 17:03:48 -0300

    twilioflex2 claim

---
+++ temba/tickets/types/twilioflex2/type.py
@@ -0,0 +1,22 @@
+from temba.tickets.models import TicketerType
+
+
+class Twilioflex2Type(TicketerType):
+    """
+    Type for using Twilio Flex 2.x as a ticketer
+    """
+
+    name = "Twilio Flex 2.x"
+    slug = "twilioflex2"
+    icon = "icon-twilio_original"
+    
+    CONFIG_ACCOUNT_SID = "account_sid"
+    CONFIG_AUTH_TOKEN = "auth_token"
+    CONFIG_FLEX_INSTANCE_SID = "flex_instance_sid"
+    CONFIG_FLEX_WORKSPACE_SID = "flex_workspace_sid"
+    CONFIG_FLEX_WORKFLOW_SID = "flex_workflow_sid"
+    
+    connect_view = None
+    
+    def is_available_to(self, user):
+        return True
