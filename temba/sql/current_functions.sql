818bd443240cdfee21ba5e20bd1126da2079c38b
Matheus Leal <matheuscardoso.pro@gmail.com>
2026-01-26 08:48:41 -0300

    feat: improve start id exclusion to avoid race condition fk error

--- temba/sql/current_functions.sql
+++ temba/sql/current_functions.sql
@@ -417,8 +417,10 @@ CREATE OR REPLACE FUNCTION
 RETURNS VOID AS $$
 BEGIN
   IF _start_id IS NOT NULL THEN
-    INSERT INTO flows_flowstartcount("start_id", "count", "is_squashed")
-    VALUES(_start_id, _count, FALSE);
+    IF EXISTS (SELECT 1 FROM flows_flowstart WHERE id = _start_id) THEN
+      INSERT INTO flows_flowstartcount("start_id", "count", "is_squashed")
+      VALUES(_start_id, _count, FALSE);
+    END IF;
   END IF;
 END;
 $$ LANGUAGE plpgsql;
