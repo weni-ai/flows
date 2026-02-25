070f8c83590d5cf51b33a6a5b00ca561c646e0a2
Robi9 <robertasousa714@gmail.com>
2026-02-25 18:25:33 -0300

    feat: Update WhatsApp broadcast interaction types to include carousel and modify quick reply to URL option

--- temba/api/v2/views.py
+++ temba/api/v2/views.py
@@ -785,7 +785,7 @@ class WhatsappBroadcastsEndpoint(ListAPIMixin, WriteAPIMixin, BaseAPIView):
                         ]
                     }
                 ],
-                "interaction_type": "flow_msg | cta_url | location | order_details",
+                "interaction_type": "flow_msg | cta_url | location | order_details | carousel",
                 "flow_message": {
                     "flow_id": "1234567890",
                     "flow_data": {
@@ -823,8 +823,8 @@ class WhatsappBroadcastsEndpoint(ListAPIMixin, WriteAPIMixin, BaseAPIView):
                                 "parameters": {"id": "item-1", "title": "Option 1"}
                             },
                             {
-                                "sub_type": "quick_reply",
-                                "parameters": {"id": "item-2", "title": "Option 2"}
+                                "sub_type": "url",
+                                "parameters": {"display_text": "Visit", "url": "https://example.com"}
                             }
                         ]
                     }
@@ -921,7 +921,7 @@ class WhatsappBroadcastsEndpoint(ListAPIMixin, WriteAPIMixin, BaseAPIView):
                         ]
                     }
                 ],
-                "interaction_type": "flow_msg | cta_url | location | order_details",
+                "interaction_type": "flow_msg | cta_url | location | order_details | carousel",
                 "flow_message": {
                     "flow_id": "1234567890",
                     "flow_data": {
